b3a67347d70203c872d321d8cf3840d11932b785
shubham423 <shubh991172@gmail.com>
2023-10-19 14:14:26 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "InstaCloneKmm"
+include(":androidApp")
+include(":shared")
