a17cfae81ec1198c901f25db3f6ac60aaf1b4a4a
shubham423 <shubh991172@gmail.com>
2023-10-20 09:18:51 +0530

    signup screen

--- androidApp/src/main/java/com/example/instaclonekmm/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/instaclonekmm/android/auth/signup/SignUpScreen.kt
@@ -1,2 +1,113 @@
 package com.example.instaclonekmm.android.auth.signup
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.instaclonekmm.android.R
+import com.example.instaclonekmm.android.common.components.CustomTextField
+import com.example.instaclonekmm.android.common.theming.ButtonHeight
+import com.example.instaclonekmm.android.common.theming.ExtraLargeSpacing
+import com.example.instaclonekmm.android.common.theming.InstaAppTheme
+import com.example.instaclonekmm.android.common.theming.LargeSpacing
+import com.example.instaclonekmm.android.common.theming.MediumSpacing
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    uiState: SignUpUiState,
+    onUsernameChange: (String) -> Unit,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onNavigateToLogin: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(
+                color = if (isSystemInDarkTheme()) {
+                    MaterialTheme.colors.background
+                } else {
+                    MaterialTheme.colors.surface
+                }
+            )
+            .padding(
+                top = ExtraLargeSpacing + LargeSpacing,
+                start = LargeSpacing + MediumSpacing,
+                end = LargeSpacing + MediumSpacing,
+                bottom = LargeSpacing
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(LargeSpacing)
+    ) {
+
+        CustomTextField(
+            value = uiState.username,
+            onValueChange = onUsernameChange,
+            hint = R.string.username_hint
+        )
+
+        CustomTextField(
+            value = uiState.email,
+            onValueChange = onEmailChange,
+            hint = R.string.email_hint,
+            keyboardType = KeyboardType.Email
+        )
+
+        CustomTextField(
+            value = uiState.password,
+            onValueChange = onPasswordChange,
+            hint = R.string.password_hint,
+            keyboardType = KeyboardType.Password,
+            isPasswordTextField = true
+        )
+
+        Button(
+            onClick = {
+                onNavigateToLogin()
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .height(ButtonHeight),
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp
+            ),
+            shape = MaterialTheme.shapes.medium
+        ) {
+            Text(text = stringResource(id = R.string.signup_button_hint))
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    InstaAppTheme() {
+        SignUpScreen(
+            uiState = SignUpUiState(),
+            onUsernameChange = {},
+            onEmailChange = {},
+            onPasswordChange = {},
+            onNavigateToLogin = {}
+        )
+    }
+}
